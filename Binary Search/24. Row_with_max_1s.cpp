539cdf033c22d60f2c7e35104c01c412354a0fdd
DEBASHIS GARAI <debashis1805@gmail.com>
2024-02-02 00:04:49 +0530

    Added brute force code for 'Row with maximum 1s'

--- Binary Search/24. Row_with_max_1s.cpp
+++ Binary Search/24. Row_with_max_1s.cpp
@@ -1,8 +1,9 @@
 // to find the row with max number of ones
+// condition is each row should contain alteast 1 zero
 #include <bits/stdc++.h>
 using namespace std;
 
-int funct(vector<vetcor<int>>& arr, int n, int m){
+int funct(vector<vector<int>>& arr, int n, int m){
   // naive solution
   // just iterate over the row and count the sum 
   // the one which is greater return that
@@ -12,13 +13,16 @@ int funct(vector<vetcor<int>>& arr, int n, int m){
     for(int j = 0; j < m; ++j){
       cnt += arr[i][j];
     }
-    if(cnt > mx){
+    if(cnt > mx && cnt != n){
       mx = cnt;
       idx = i;
     }
   }
-  return i;
+  return idx;
 }
 int main() {
-  
+  vector<vector<int>>vv = {{0, 1, 1}, {1,1,1}, {0, 0, 1}};
+  int m = vv[0].size();
+  int n = vv.size();
+  cout << "The row is: " << funct(vv, n, m);
 }
